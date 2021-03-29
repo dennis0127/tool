09e90184a02c46e95bfe79f455ba0994cde9bf22
lxd <ilovelxd@163.com>
2021-03-29 11:02:50 +0800

    sw list

--- src/main/java/com/yyyow/tool/controller/SwController.java
+++ src/main/java/com/yyyow/tool/controller/SwController.java
@@ -3,19 +3,18 @@ package com.yyyow.tool.controller;
 import com.yyyow.tool.util.R;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequestMapping("/api/sw")
 @Api(tags = "sw Account List")
 public class SwController {
 
 
-
     @ApiOperation(value = "list", notes = "list", httpMethod = "GET")
     @GetMapping
+    @ResponseBody
     public R list() {
 
 
